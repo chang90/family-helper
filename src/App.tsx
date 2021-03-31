3a8bfc7d7d5064adc0a9e629b9edc4fed4b5f313
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-31 21:49:31 +1100

    Create modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import './App.css';
 import Amplify from 'aws-amplify';
 import awsConfig from './aws-exports';
@@ -7,7 +7,7 @@ import { listLists } from './graphql/queries';
 import callGraphQL from './graphql/graphql-api';
 import MainHeader from './components/headers/MainHeader';
 import Lists from './components/list/Lists';
-import { Button, Container, Icon } from 'semantic-ui-react';
+import { Button, Container, Form, Icon, Modal } from 'semantic-ui-react';
 
 
 interface ListData {
@@ -31,8 +31,29 @@ interface ListItem {
 
 Amplify.configure(awsConfig);
 
+const initialState = {
+  title: '',
+  description: ''
+}
+
+function listReducer(state = initialState, action: {type: string, value: string}) {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return {...state, description: action.value};
+    case 'TITLE_CHANGED':
+      return {...state, title: action.value};
+    default:
+      console.log('Default action for: ', action)
+      return state
+  }
+}
+
 function App() {
+
+
+  const [state, dispatch] = useReducer(listReducer, initialState);
   const [lists, setLists] = useState<ListItem[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const fetchList = async () => {
     const { data } = await callGraphQL<ListData>(listLists);
     if (data?.listLists?.items) {
@@ -45,16 +66,42 @@ function App() {
     fetchList()
   }, []);
 
+  const toggleModal = (shouldOpen: boolean) => {
+    setIsModalOpen(shouldOpen);
+  }
+
   return (
     <AmplifyAuthenticator>
-      <AmplifySignOut />
-      <Button className='floatingButton'>
-        <Icon name='plus' className='floatingButton_icon' />
-      </Button>
       <Container>
+        <AmplifySignOut />
+        <Button className='floatingButton' onClick={() => toggleModal(true)}>
+          <Icon name='plus' className='floatingButton_icon' />
+        </Button>
         <MainHeader />
         <Lists lists={lists} />
       </Container>
+      <Modal open={isModalOpen} dimmer='inverted'>
+        <Modal.Header>Create your list</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Input
+              error={true ? false : { content: 'Please add a name to your list' }}
+              label="Title"
+              placeholder="My pretty list"
+              value={state.title}
+              onChange={(e) => dispatch({ type: 'TITLE_CHANGED', value: e.target.value })}></Form.Input>
+            <Form.TextArea
+              label='Description'
+              placeholder='Things that my pretty list is about'
+              value={state.description}
+              onChange={(e) => dispatch({ type: 'DESCRIPTION_CHANGED', value: e.target.value })}></Form.TextArea>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button negative onClick={() => { toggleModal(false) }}>Cancel</Button>
+          <Button positive onClick={() => { toggleModal(false) }}>Save</Button>
+        </Modal.Actions>
+      </Modal>
     </AmplifyAuthenticator>
   );
 }
